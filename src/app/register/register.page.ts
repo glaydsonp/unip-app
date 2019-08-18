b49485db58bc4548f0a8f0f81192080ba7773185
Glaydson Prado <glaydsonp@hotmail.com>
2019-08-17 21:39:06 -0300

    firebase register done, realtime db online done

--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
+import * as firebase from 'firebase';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { ToastController } from "@ionic/angular";
 
 @Component({
   selector: 'app-register',
@@ -7,9 +11,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterPage implements OnInit {
 
-  constructor() { }
+
+  constructor(private router: Router, public firebaseauth: AngularFireAuth, public toastCtrl: ToastController) { }
 
   ngOnInit() {
   }
 
+  register(form) {
+    this.firebaseauth.auth.createUserWithEmailAndPassword(form.value.email, form.value.password)
+      .then(() => {
+        firebase.database().ref('alunos/' + firebase.auth().currentUser.uid).set({
+          nome: form.value.nome,
+          matricula: form.value.matricula,
+          turma: form.value.turma,
+          email: form.value.email
+        }).then(() => {
+          this.exibirToast('Usuário criado com sucesso');
+        });
+      })
+      .catch((erro: any) => {
+        this.exibirToast(erro);
+      });
+  }
+
+  private exibirToast(mensagem: string): void {
+    let toast = this.toastCtrl.create({
+      duration: 3000,
+      position: 'bottom'
+    });
+  }
 }
